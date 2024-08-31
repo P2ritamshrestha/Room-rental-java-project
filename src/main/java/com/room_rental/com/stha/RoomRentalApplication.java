b9e9b4835320ece15ed968fbde022201533bd6f2
Pritam Shrestha <p2ritamsthagrk@gmail.com>
2024-08-31 22:56:57 +0545

    project setup complete

---
+++ src/main/java/com/room_rental/com/stha/RoomRentalApplication.java
@@ -0,0 +1,13 @@
+package com.room_rental.com.stha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/room_rental/com/stha/RoomRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.room_rental.com.stha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
