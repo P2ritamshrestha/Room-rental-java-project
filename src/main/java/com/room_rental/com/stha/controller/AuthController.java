b6bc2a350daf0a7322a6c97c785438aab59b53b6
Pritam Shrestha <p2ritamsthagrk@gmail.com>
2024-09-03 22:55:16 +0545

    add @CrossOrigin(origins = {http://localhost:5173, http://localhost:5174})

--- src/main/java/com/room_rental/com/stha/controller/AuthController.java
+++ src/main/java/com/room_rental/com/stha/controller/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174"})
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
--- src/main/java/com/room_rental/com/stha/controller/UserController.java
+++ src/main/java/com/room_rental/com/stha/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174"})
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/landlord")
