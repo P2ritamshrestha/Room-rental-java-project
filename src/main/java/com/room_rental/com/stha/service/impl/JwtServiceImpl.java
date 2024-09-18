1654686c0897d59f61204b7d4323df1bc37cc5e1
Pritam Shrestha <p2ritamsthagrk@gmail.com>
2024-09-18 11:04:37 +0545

    upgrade jwt token expiration time

--- src/main/java/com/room_rental/com/stha/service/impl/JwtServiceImpl.java
+++ src/main/java/com/room_rental/com/stha/service/impl/JwtServiceImpl.java
@@ -20,7 +20,7 @@ public class JwtServiceImpl implements JwtService {
     public String generateToken(String email) {
         return Jwts.builder().setSubject(email)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 10000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000000 * 60 * 24))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
@@ -30,7 +30,7 @@ public class JwtServiceImpl implements JwtService {
         return Jwts.builder()
                 .setClaims(extraClaims).setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 604800000))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000000 * 60 * 24))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
